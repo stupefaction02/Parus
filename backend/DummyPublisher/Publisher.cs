ed32a1b96c8e0ac735b64f158fa3226c6eee8479
Ivan <ivan.safonow2012@yandex.ru>
2020-08-30 20:36:25 +0300

    Fix file uploading, adding exception handling

--- backend/DummyPublisher/Publisher.cs
+++ backend/DummyPublisher/Publisher.cs
@@ -13,6 +13,7 @@ using Naturistic.Core.Extensions;
 using Newtonsoft.Json;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace DummyPublisher
 {
@@ -22,9 +23,13 @@ namespace DummyPublisher
         /// Channel session key. Retrieved a new one from the server everytime publisher starts broadcasting.
         /// Is used in crypting segment file name with hash function
         /// </summary>
-        private string chSessionKey;
+        private string channelSessionKey;
 
         private bool isRecording = false;
+
+        private int channelId;
+
+        private int[] availableBitrates = new int[1] { 1000 };
 
         /// <summary>
         /// URL of the local HSL microservice
@@ -33,9 +38,14 @@ namespace DummyPublisher
 
         private readonly string mainServerApiUrl = "https://localhost:3939/api";
 
-        private readonly System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
+        private readonly MD5 md5 = MD5.Create();
 
         private HttpClient client = new HttpClient();
+
+        /// <summary>
+        /// Base HLS url
+        /// </summary>
+        private readonly string bhlsurl = "https://localhost:2020/hls/live";
 
         // test
         private string[] files = new string[5]
@@ -55,12 +65,11 @@ namespace DummyPublisher
 
             // 3. Start session and and store it o a server, then get channel key, that represts that info
             // TODO: Get id from local store
-            int channelId = 1;
+            channelId = 1;
             Console.WriteLine("Trying to retrive channel key...");
 
             try
             {
-
                 var resString = client.GetStringAsync($"{mainServerApiUrl}/broadcasts/startSession?channelId={channelId}").Result;
 
                 if (!String.IsNullOrEmpty(resString))
@@ -70,21 +79,11 @@ namespace DummyPublisher
 
                     if (raw != null)
                     {
-                        chSessionKey = raw["channelSessionKey"];
+                        channelSessionKey = raw["channelSessionKey"];
 
-                        // 4. Create manifest for the different bitrates
-                        // Since we are in a test mode, we got only 1000kbps
+                        Task.Run(() => InitManifestsAsync());
 
-                        // Create master manifest
-                        var mmbuilder = new ManifestBuilder(@$"video/master-manifest-{channelId}.m3u8");
-                        mmbuilder = mmbuilder.AddExtStreamInfo("https://localhost:5001/1000K/playlist-1000K.m3u8", 1000,
-                            "mp4a.40.2", "avc1.640028", new Rectangle { X = 640, Y = 480 });
-
-                        var lmt = Task.Run(async () => await PostFileAsync(mmbuilder.Build(), 1));
-
-                        lmt.Wait();
-
-                        Console.WriteLine($"Starting session... key: {chSessionKey}");
+                        Console.WriteLine($"Starting session... key: {channelSessionKey}");
                     }
                 }
                 else
@@ -101,14 +100,124 @@ namespace DummyPublisher
             }
         }
 
+        private async Task InitManifestsAsync()
+        {
+            // 4. Create manifest for the different bitrates
+            // Since we are in a test mode, we got only 1000kbps
+
+            // Create master manifest
+            var masterManifestBuilder = new ManifestBuilder(@$"video/master-manifest-{channelId}.m3u8");
+
+            foreach (var abitrate in availableBitrates)
+            {
+                var pattern = $"{channelSessionKey}manifest{abitrate}k";
+
+                var fileNameHash = md5.CreateHash(pattern);
+                var fn = $"{fileNameHash}.m3u8";
+
+                masterManifestBuilder = masterManifestBuilder.AddExtStreamInfo($"{bhlsurl}/files?fn={fn}", abitrate,
+                    "mp4a.40.2", "avc1.640028", new Rectangle { X = 640, Y = 480 });
+
+                var manifestBuilder = new ManifestBuilder(Path.Combine(@"video/1000k", fn));
+                manifestBuilder.AddVersion(3).AddTargetDuration(10).AddMediaSequence(0);
+
+                foreach (var segment in this.files)
+                {
+                    manifestBuilder.AddExtInf(segment, 1.0000);
+                }
+
+                manifestBuilder.AddEndPlaylist();
+
+                var pfit = PostFileInternalAsync(manifestBuilder.Build(), fn);
+                await pfit.ContinueWith(t =>
+                {
+                    if (t.IsFaulted)
+                        masterManifestBuilder.Dispose();
+                });
+            }
+
+            var pft = PostFileAsync(masterManifestBuilder.Build(), "master_manifest");
+            await pft.ContinueWith(t =>
+            {
+                if (t.IsFaulted)
+                    masterManifestBuilder.Dispose();
+            });
+        }
+
+        private async Task PostFileAsync(string fp, string fileSalt)
+            => await PostFileAsync(File.Open(fp, FileMode.OpenOrCreate, FileAccess.Read), fileSalt);
+
+        /// <summary>
+        /// Post file on a HLS service.  
+        /// </summary>
+        /// <param name="fs">Stream of a file</param>
+        /// <param name="fileSalt">Salt added to a md5 encryption. Used in a client to decrypt filename</param>
+        private async Task PostFileAsync(FileStream fs, string fileSalt = "")
+        {
+            var fileName = $"{channelSessionKey}{fileSalt}";
+
+            // #!
+            // Matter to note, that in order to get the hash identicate to http://www.md5.cz/ and
+            // CryptoJS library hashes we must set ASCII encoding
+            string fileNameHash = md5.CreateHash(fileName);
+            fileNameHash = $"{fileNameHash}{Path.GetExtension(fs.Name)}";
+
+            var task = PostFileInternalAsync(fs, fileNameHash);
+            await task.ContinueWith(t =>
+            {
+                if (t.IsFaulted)
+                    fs.Dispose();
+            });
+        }
+
+        /// <summary>
+        /// Posts file
+        /// </summary>
+        /// <param name="fs"></param>
+        /// <param name="fileNameHash">Hashed filename with extension</param>
+        /// <returns></returns>
+        private async Task PostFileInternalAsync(FileStream fs, string fileNameHash)
+        {
+            if (fs != null)
+            {
+                if (!fs.CanRead)
+                    throw new InvalidOperationException("FileStream aren't supposed to be unreadable.");
+
+                string dest = $"{localHlsMServiceUrl}/hls/live/files";
+
+                using (var content = new MultipartFormDataContent())
+                {
+                    content.Add(new StreamContent(fs), "segmentFile", fileNameHash);
+
+                    var request = new HttpRequestMessage(HttpMethod.Post, dest);
+
+                    Console.WriteLine($"Sending file... name: {fileNameHash}, file: {Path.GetFileName(fs.Name)}, destination: {dest}");
+
+                    request.Headers.Add("Cache-Control", "no-cache");
+                    request.Headers.Add("Accept", "*/*");
+                    request.Headers.Add("Accept-Encoding", "gzip, deflate, br");
+                    request.Content = content;
+
+                    // we don't care about answer 
+                    await client.SendAsync(request);
+                }
+
+                // TODO:
+                // You know, I can't come up an elegant solution that relates the fact that if sendAsync throws an exception
+                // the file stream will left undisposed
+
+                await fs.DisposeAsync();
+            }
+        }
+
         public void Start() => Task.Run(async () => await StartAsync());
 
         public async Task StartAsync()
         {
             isRecording = true;
 
-            int fileIndex = 1;
-            int _i = 0; // :)
+            int fileSalt = 1;
+            int fileSegmentIndex = 0;
 
             Console.WriteLine("type: HttpClient");
 
@@ -116,74 +225,21 @@ namespace DummyPublisher
             {
                 while (isRecording)
                 {
-                    await PostFileAsync(files[_i], fileIndex);
+                    await PostFileAsync(files[fileSegmentIndex], fileSalt.ToString());
 
-                    fileIndex += 1;
-                    _i++;
+                    fileSalt += 1;
+                    fileSegmentIndex++;
 
-                    if (_i == files.Length)
-                        _i = 0;
+                    if (fileSegmentIndex == files.Length)
+                        fileSegmentIndex = 0;
 
-                    Thread.Sleep(3000);
+                    Thread.Sleep(500);
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine($"{e.GetType().Name}: {e.Message}");
             }
-        }
-
-        private async Task PostFileAsync(FileStream fs, int fileIndex)
-        {
-            if (fs != null)
-            {
-                try
-                {
-                    var fileName = $"{chSessionKey}{fileIndex}";
-
-                    byte[] retVal = md5.ComputeHash(Encoding.Unicode.GetBytes(fileName));
-                    StringBuilder sb = new StringBuilder();
-
-                    for (int i = 0; i < retVal.Length; i++)
-                        sb.Append(retVal[i].ToString("x2"));
-
-                    string dest = $"{localHlsMServiceUrl}/hls/live/files";
-
-                    using (var content = new MultipartFormDataContent())
-                    {
-                        // TODO: Add disposal support for fs
-                        string nfn = $"{sb}{Path.GetExtension(fs.Name)}";
-
-                        content.Add(new StreamContent(fs), "SegmentFile", nfn);
-
-                        var request = new HttpRequestMessage(HttpMethod.Post, dest);
-
-                        Console.WriteLine($"Sending file... name: {nfn}, file: {Path.GetFileName(fs.Name)}, destination: {dest}");
-
-                    //    request.Headers.Add("Cache-Control", "no-cache");
-                        request.Headers.Add("Accept", "*/*");
-                        request.Headers.Add("Accept-Encoding", "gzip, deflate, br");
-                        request.Content = content;
-
-                        // we don't care about answer 
-                        await client.SendAsync(request);
-                    }
-                }
-                catch (HttpRequestException hrex)
-                {
-                    Console.WriteLine($"Can't connect to the HLS service. It might be that the server is shutdown right now.");
-                    return;
-                }
-                catch (NotSupportedException nsex)
-                {
-                    var y = nsex.Message;
-                }
-            }
-        }
-
-        private async Task PostFileAsync(string fp, int fileIndex)
-        {
-            await PostFileAsync(File.Open(fp, FileMode.OpenOrCreate, FileAccess.Read), fileIndex);
         }
 
         public void Dispose()
@@ -219,6 +275,16 @@ namespace DummyPublisher
             manifestWriter.WriteLine("");
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filepath"></param>
+        /// <param name="type">Type of playlist. Can be either VOD or EVENT</param>
+        public ManifestBuilder(string filepath, string type) : this(filepath)
+        {
+            manifestWriter.WriteLine($"#EXT-X-PLAYLIST-TYPE:{type}");
+        }
+
         public ManifestBuilder AddExtStreamInfo(string url, int bandwidth, string vcodec, string acodec, Rectangle resolution)
         {
             //# EXTM3U
@@ -241,6 +307,38 @@ namespace DummyPublisher
             return this;
         }
 
+        public ManifestBuilder AddExtInf(string info, double duration)
+        {
+            manifestWriter.WriteLine($"#EXTINF:{duration}");
+            manifestWriter.WriteLine(info);
+
+            return this;
+        }
+
+        public ManifestBuilder AddEndPlaylist()
+        {
+            manifestWriter.WriteLine($"#EXT-X-ENDLIST");
+            return this;
+        }
+
+        public ManifestBuilder AddMediaSequence(int sequence)
+        {
+            manifestWriter.WriteLine($"#EXT-X-MEDIA-SEQUENCE:{sequence}");
+            return this;
+        }
+
+        public ManifestBuilder AddTargetDuration(int duration)
+        {
+            manifestWriter.WriteLine($"#EXT-X-TARGETDURATION:{duration}");
+            return this;
+        }
+
+        public ManifestBuilder AddVersion(int version)
+        {
+            manifestWriter.WriteLine($"EXT-X-VERSION:{version}");
+            return this;
+        }
+
         public FileStream Build()
         {
             // Shitcoding here we see
@@ -250,15 +348,40 @@ namespace DummyPublisher
             // TODO: Try to set FileAccess in StremaWriter at the very begin
             manifestWriter.Flush();
             var fs = manifestWriter.BaseStream as FileStream;
+            fs.Dispose();
             fs.Close();
 
             return new FileStream(fs.Name, FileMode.OpenOrCreate, FileAccess.ReadWrite);
         }
 
+        #region Dispose
         public void Dispose()
         {
             if (manifestWriter != null)
                 manifestWriter.Dispose();
+        }
+
+        #endregion
+    }
+
+    public static class CryptoExtensions
+    {
+        /// <summary>
+        /// Uses ASCII encoding
+        /// </summary>
+        /// <param name="md5"></param>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string CreateHash(this MD5 md5, string str)
+        {
+            var bytes = Encoding.ASCII.GetBytes(str);
+            var retVal = md5.ComputeHash(bytes);
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < retVal.Length; i++)
+                sb.Append(retVal[i].ToString("x2"));
+
+            return sb.ToString();
         }
     }
 }
